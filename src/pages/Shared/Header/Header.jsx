1c027279f9a24e703b0db9e895ccdffd7a7b63bf
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-02 11:34:23 +0600

    react-fast-marquee added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
+import { Button, Container } from 'react-bootstrap';
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
     return (
-        <div>
+        <Container>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p className='text-secondary'>Journalism Without Fear or Favour</p>
                 <p>{moment().format("dddd, MMMM DD, YYYY")}</p>
             </div>
-        </div>
+            <div>
+                <Button variant='danger'>Latest</Button>
+                <Marquee speed={50}>
+                    I can be a React component, multiple React components, or just some text.
+                </Marquee>
+            </div>
+        </Container>
     );
 };
 
