9cd26b8052267be2e8293022de502a282b52f3d6
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-05-03 16:06:19 +0600

    vercel server added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -41,12 +41,12 @@ const router = createBrowserRouter([
             {
                 path: '/category',
                 element: <Category></Category>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://the-news-dragon-server-minhajul9.vercel.app/news')
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/categories/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-minhajul9.vercel.app/categories/${params.id}`)
             }
         ]
     },
@@ -57,7 +57,7 @@ const router = createBrowserRouter([
             {
                 path: ':id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader: ({params}) => fetch(`https://the-news-dragon-server-minhajul9.vercel.app/news/${params.id}`)
             }
         ]
     }
